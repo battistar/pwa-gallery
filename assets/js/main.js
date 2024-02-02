3885a761d31c6a9b56885beabed4902164820d8b
Samuele Battistella <battistar92@gmail.com>
2024-02-02 23:50:54 +0100

    Add installation date info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,39 @@
-async function registerServiceWorker() {
+const setInstallDate = () => {
+  const installDate = localStorage.getItem('installDate');
+
+  if (!installDate) {
+    localStorage.setItem('installDate', new Date().toISOString());
+  }
+};
+
+const getInstallDate = () => {
+  return localStorage.getItem('installDate');
+};
+
+const displayInstallDate = () => {
+  const strong = document.querySelector('p strong');
+
+  const installDateISO = getInstallDate();
+
+  if (installDateISO) {
+    const installDate = new Date(installDateISO);
+    const currentDate = new Date();
+
+    const diffTime = Math.abs(installDate - currentDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    strong.textContent = diffDays;
+  } else {
+    strong.textContent = 0;
+  }
+};
+
+const handleInstallDate = () => {
+  setInstallDate();
+  displayInstallDate();
+};
+
+const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
       await navigator.serviceWorker.register(`/pwa-gallery/sw.js`);
@@ -8,8 +43,10 @@ async function registerServiceWorker() {
       console.error(error);
     }
   }
-}
+};
 
 window.addEventListener('load', async () => {
   await registerServiceWorker();
+
+  handleInstallDate();
 });
